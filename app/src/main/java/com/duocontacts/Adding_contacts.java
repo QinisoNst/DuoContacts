9d57f62b32548994ba9c2f814a156a7a5ca8df76
Qiniso Nst <qmolefe1518@gmail.com>
2023-09-16 23:21:20 +0200

    Project2

---
+++ app/src/main/java/com/duocontacts/Adding_contacts.java
@@ -0,0 +1,14 @@
+package com.duocontacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Adding_contacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_contacts);
+    }
+}
---
+++ app/src/main/java/com/duocontacts/Contact_Info.java
@@ -0,0 +1,14 @@
+package com.duocontacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contact_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info);
+    }
+}
